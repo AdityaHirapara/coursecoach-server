5dacdfe5f80f5e1a94547a10b6527b15b0d2fc78
AdityaHirapara <adityahirapara2016@gmail.com>
2018-03-26 22:08:39 +0530

    :wrench:configure mongodb models

---
+++ models/subject.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const SubjectSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  topics: [{ type: Schema.Types.ObjectId, ref: 'Topic' }]
+});
+
+module.exports = mongoose.model('Subject',SubjectSchema);
