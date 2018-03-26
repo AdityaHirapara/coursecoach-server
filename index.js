2a2d6db1d6fb4cdfa64ba354d554bfb9c4b5bffc
AdityaHirapara <adityahirapara2016@gmail.com>
2018-03-26 22:08:39 +0530

    configure mongodb models

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const Course = require('./models/course');
+const Subject = require('./models/subject');
+const Topic = require('./models/topic');
+
+mongoose.connect('link');
 
 server.listen(port, () =>{
   console.log(`Server is running on ${port}`);
---
+++ models/course.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CourseSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  subjects: [{ type: Schema.Types.ObjectId, ref: 'Subject' }]
+});
+
+module.exports = mongoose.model('Course',CourseSchema);
